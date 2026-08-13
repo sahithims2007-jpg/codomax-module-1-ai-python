4062bbd4c83a59529134bf3d89a5f5e716fe7740
sahithims2007-jpg <sahithims2007@gmail.com>
2026-08-13 22:00:05 +0530

    Add function to calculate the square of a number

---
+++ 04_functions.py
@@ -0,0 +1,11 @@
+# Functions in Python
+
+def calculate_square(number):
+    return number * number
+
+
+num = float(input("Enter a number: "))
+
+result = calculate_square(num)
+
+print("Square:", result)
