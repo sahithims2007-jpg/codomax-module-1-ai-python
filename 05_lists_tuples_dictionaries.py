bd626cf3df9d1ece31352f16767cc4ce10a4bb17
sahithims2007-jpg <sahithims2007@gmail.com>
2026-08-13 22:00:51 +0530

    Implement examples of Python data structures
    Added examples of list, tuple, and dictionary in Python.

---
+++ 05_lists_tuples_dictionaries.py
@@ -0,0 +1,22 @@
+# Python Data Structures
+
+# List
+languages = ["Python", "C", "Java", "JavaScript"]
+
+print("Languages:", languages)
+
+# Tuple
+coordinates = (10, 20)
+print("Coordinates:", coordinates)
+
+# Dictionary
+student = {
+    "name": "Sahithi",
+    "branch": "CSE (AI & ML)",
+    "module": 1
+}
+
+print("Student Details:")
+print("Name:", student["name"])
+print("Branch:", student["branch"])
+print("Module:", student["module"])
